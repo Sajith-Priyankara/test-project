55cf03f90c3da04041c31971a3c9c3051d63d3c0
Sajith-Priyankara <e19304@eng.pdn.ac.lk>
2023-06-21 17:50:14 +0530

    second\ one

--- Upload.java
+++ Upload.java
@@ -1,5 +1,6 @@
 public class Upload {
     public static void main(String[] args) {
         System.out.println("Git hello this first one ");
+        System.out.println("this is the second one");
     }
 }
